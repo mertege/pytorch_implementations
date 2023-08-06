3548a6f915872d0517ae0d50f727a8c49e58a33a
Mert Ege <31161584+mertege@users.noreply.github.com>
2023-08-06 16:32:54 +0300

    Create embedding.py

---
+++ Bert/embedding.py
@@ -0,0 +1,50 @@
+class PositionalEmbedding(torch.nn.Module):
+
+    def __init__(self, d_model, max_len=128):
+        super().__init__()
+
+        # Compute the positional encodings once in log space.
+        pe = torch.zeros(max_len, d_model).float()
+        pe.require_grad = False
+
+        for pos in range(max_len):   
+            # for each dimension of the each position
+            for i in range(0, d_model, 2):   
+                pe[pos, i] = math.sin(pos / (10000 ** ((2 * i)/d_model)))
+                pe[pos, i + 1] = math.cos(pos / (10000 ** ((2 * (i + 1))/d_model)))
+
+        # include the batch size
+        self.pe = pe.unsqueeze(0)   
+        # self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        return self.pe
+
+class BERTEmbedding(torch.nn.Module):
+    """
+    BERT Embedding which is consisted with under features
+        1. TokenEmbedding : normal embedding matrix
+        2. PositionalEmbedding : adding positional information using sin, cos
+        2. SegmentEmbedding : adding sentence segment info, (sent_A:1, sent_B:2)
+        sum of all these features are output of BERTEmbedding
+    """
+
+    def __init__(self, vocab_size, embed_size, seq_len=64, dropout=0.1):
+        """
+        :param vocab_size: total vocab size
+        :param embed_size: embedding size of token embedding
+        :param dropout: dropout rate
+        """
+
+        super().__init__()
+        self.embed_size = embed_size
+        # (m, seq_len) --> (m, seq_len, embed_size)
+        # padding_idx is not updated during training, remains as fixed pad (0)
+        self.token = torch.nn.Embedding(vocab_size, embed_size, padding_idx=0)
+        self.segment = torch.nn.Embedding(3, embed_size, padding_idx=0)
+        self.position = PositionalEmbedding(d_model=embed_size, max_len=seq_len)
+        self.dropout = torch.nn.Dropout(p=dropout)
+       
+    def forward(self, sequence, segment_label):
+        x = self.token(sequence) + self.position(sequence) + self.segment(segment_label)
+        return self.dropout(x)
